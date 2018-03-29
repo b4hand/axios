4b454f792bc32a6c7dd1418111c6e7898f5de51a
Brandon Forehand <b4hand@users.sf.net>
2018-03-28 20:44:55 -0700

    Adding responseUrl (for http) and responseURL (for xhr).

--- test/unit/adapters/http.js
+++ test/unit/adapters/http.js
@@ -85,6 +85,7 @@ module.exports = {
       axios.get('http://localhost:4444/one').then(function (res) {
         test.equal(res.data, str);
         test.equal(res.request.path, '/two');
+        test.equal(res.responseUrl, 'http://localhost:4444/two');
         test.done();
       });
     });
